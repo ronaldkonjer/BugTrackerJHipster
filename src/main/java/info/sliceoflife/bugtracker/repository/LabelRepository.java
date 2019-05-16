609d192b4d6c9f6d24ba5b73ee7e228b60bd5e24
Ronald Konjer <ronaldkonjer@gmail.com>
2019-05-16 22:03:01 +0200

    blabla

---
+++ src/main/java/info/sliceoflife/bugtracker/repository/LabelRepository.java
@@ -0,0 +1,15 @@
+package info.sliceoflife.bugtracker.repository;
+
+import info.sliceoflife.bugtracker.domain.Label;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Label entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface LabelRepository extends JpaRepository<Label, Long> {
+
+}
---
+++ src/main/java/info/sliceoflife/bugtracker/repository/ProjectRepository.java
@@ -0,0 +1,15 @@
+package info.sliceoflife.bugtracker.repository;
+
+import info.sliceoflife.bugtracker.domain.Project;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Project entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
